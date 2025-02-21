fa5318e435892238189ee3ef72776bb6bc138145
deadmanShadow <raihanshamil33@gmail.com>
2025-02-21 21:10:28 +0600

    initial commit

---
+++ app/layout.js
@@ -0,0 +1,22 @@
+import { AppContextProvider } from "@/context/AppContext";
+import { Outfit } from "next/font/google";
+import { Toaster } from "react-hot-toast";
+import "./globals.css";
+
+const outfit = Outfit({ subsets: ["latin"], weight: ["300", "400", "500"] });
+
+export const metadata = {
+  title: "Nexus Gadgets",
+  description: "E-Commerce with Next.js ",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${outfit.className} antialiased text-gray-700`}>
+        <Toaster />
+        <AppContextProvider>{children}</AppContextProvider>
+      </body>
+    </html>
+  );
+}
